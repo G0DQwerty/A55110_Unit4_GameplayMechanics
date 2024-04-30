301f7b52eb1a0f0035b54414aba94782cf426608
David Figueiredo <150687060+G0DQwerty@users.noreply.github.com>
2024-04-30 15:09:54 +0100

    Unit4_Part2

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float speed;
+    private Rigidbody enemyRb;
+    private GameObject player;
+
+    void Start()
+    {
+        enemyRb = GetComponent<Rigidbody>();
+        player = GameObject.Find("Player");
+    }
+
+
+    void Update()
+    {
+        //Step 4.2 enemyRb.AddForce((player.transform.position - transform.position).normalized * speed);
+        Vector3 lookDirection = (player.transform.position - transform.forward).normalized;
+    }
+}
